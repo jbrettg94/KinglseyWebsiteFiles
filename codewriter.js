b4200b55f04ca778b3cd901b7e94ca8f09f1fe1a
jbrettg94 <jbrettgalbreath94@gmail.com>
2021-12-07 15:46:25 -0500

    fixed navicons in footer with padding

--- codewriter.js
+++ codewriter.js
@@ -1,14 +1,4 @@
-let inputCode = `<img src="images/traditionals/Charcoal_Still_Life_With_Fur.jpg" class="image-gallery"/>
-<img src="images/traditionals/Gouache_Painting_Man_With_Cub.jpg" class="image-gallery"/>
-<img src="images/traditionals/Graphite_Still_Life_In_Scenery.jpg" class="image-gallery"/>
-<img src="images/traditionals/IMG_3973.JPG" class="image-gallery"/>
-<img src="images/traditionals/Kingsley-Wentink_2DDesignFall2018.png" class="image-gallery"/>
-<img src="images/traditionals/Pen_Patterns_Inside_Shapes.jpg" class="image-gallery"/>
-<img src="images/traditionals/Pen_Radial_Symmetry.jpg" class="image-gallery"/>
-<img src="images/traditionals/Pen_Symmetrical_Drawing.jpg" class="image-gallery"/>
-<img src="images/traditionals/Prismacolor_Color_Pencils_Surrealism.jpg" class="image-gallery"/>
-<img src="images/traditionals/traditional_one.JPG" class="image-gallery"/>
-<img src="images/traditionals/Wentink_Final_Painting.jpg" class="image-gallery"/>`;
+let inputCode = ``;
 
 const createArray = (string) => {
     let newArray = string.split("\n");
